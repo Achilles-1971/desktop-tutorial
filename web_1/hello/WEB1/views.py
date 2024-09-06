d364ffa16c25e071c7743b10f7d91a84cdaf3f9c
Achilles, son of Peleus <saikinairiofficial@gmail.com>
2024-09-06 13:10:47 +0300

    LZ6_13:10

--- web_1/hello/WEB1/views.py
+++ web_1/hello/WEB1/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render
+from django.template.response import TemplateResponse
+from django.template.response import TemplateResponse
 def index(request):
-    return HttpResponse("Index")
+    header = "Персональные данные" # обычная переменная
+    langs = ["Английский", "Немецкий", "Испанский"] # массив
+    user = {"name": "Максим,", "age": 30} # словарь
+    addr = ("Виноградная", 23, 45) # кортеж
+    data = {"header": header, "langs": langs, "user": user, "address":
+    addr}
+    return TemplateResponse(request, "index.html", data)
 def about(request):
     return HttpResponse("About")
 def contact(request):
