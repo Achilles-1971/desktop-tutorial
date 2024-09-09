89b2845001c6cdab152b973d325f33bdd37500a1
Achilles, son of Peleus <saikinairiofficial@gmail.com>
2024-09-09 12:19:29 +0300

    LZ8_12:19

--- web_1/hello/WEB1/views.py
+++ web_1/hello/WEB1/views.py
@@ -4,14 +4,8 @@ from django.template.response import TemplateResponse
 from django.http import *
 from WEB1.forms import UserForm
 def index(request):
-    if request.method == "POST":
-        name = request.POST.get("name") # получить значения поля Имя
-        age = request.POST.get("age") # значения поля Возраст
-        output = "<h2>Пользователь</h2><h3>Имя - {0}, Возраст – {1}</h3>".format(name, age)
-        return HttpResponse(output)
-    else:
-        userform = UserForm()
-        return render(request, "WEB1/index.html", {"form": userform})
+    userform = UserForm()
+    return render(request, "WEB1/index.html", {"form": userform})
 def about(request):
     return HttpResponse("About")
 def contact(request):
