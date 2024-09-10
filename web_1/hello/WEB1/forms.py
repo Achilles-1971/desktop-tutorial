4d985d4437122cb14186621979016c3e0335c5c5
Achilles, son of Peleus <saikinairiofficial@gmail.com>
2024-09-10 12:42:23 +0300

    PZ5_12:42

--- web_1/hello/WEB1/forms.py
+++ web_1/hello/WEB1/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 class UserForm(forms.Form):
-    name = forms.CharField(label="Имя клиента", max_length=15, help_text="ФИO не более 15 символов")
-    email = forms.EmailField(label="Электронный адрес", help_text="Обязательный символ - @")
-    ip_adres = forms.GenericIPAddressField(label="IP адрес", help_text=" Пример формата 192.0.2.0")
-    reg_text = forms.RegexField(label="Текст", regex="^[0-9][A-F][0-9]$")
-    slug_text = forms.SlugField(label="Введите текст")
-    url_text = forms.URLField(label="Введите URL", help_text="Например http://www.google.com")
-    uuid_text = forms.UUIDField(label="Введите UUID", help_text="Формат xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
-    combo_text = forms.ComboField(label="Введите URL", fields=[forms.URLField(), forms.CharField(max_length=20)])
-    file_path = forms.FilePathField(label="Выберите файл", path="C:/Python37/",allow_files="True", allow_folders="True")
-    file = forms.FileField(label="Файл")
+    file = forms.ImageField(label="Изображение")
+    date = forms.DateField(label="Введите дату")
+    time = forms.DateField(label="Введите время")
+    time_delta = forms.DurationField(label="Введите промежуток времени")
+    date_time = forms.SplitDateTimeField(label="Введите дату и время")
+    num = forms.IntegerField(label="Введите целое число")
+    num = forms.DecimalField(label="Введите десятичное число", decimal_places=2)
+    ling = forms.ChoiceField(label="Выберите язык", choices=((1, "Английский"),(2, "Немецкий"),
+    (3, "Французский")))
+    city = forms.TypedMultipleChoiceField(label="Выберите город",empty_value=None,choices=((1, "Москва"),
+    (2, "Воронеж"),(3, "Курск"),(4, "Томск")))
+    country = forms.MultipleChoiceField(label="Выберите страны",choices=((1, "Англия"),(2, "Германия"),
+    (3, "Испания"),(4, "Россия")))
