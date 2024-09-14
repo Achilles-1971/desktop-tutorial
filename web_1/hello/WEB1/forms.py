8e88f7f45b3a5b79d272ae151420ef2c27a108a6
Achilles, son of Peleus <saikinairiofficial@gmail.com>
2024-09-14 18:05:04 +0300

    LZ9_18:04_2

--- web_1/hello/WEB1/forms.py
+++ web_1/hello/WEB1/forms.py
@@ -2,4 +2,5 @@ from django import forms
 class UserForm(forms.Form):
     name = forms.CharField(label="Имя")
     age = forms.IntegerField(label="Возраст")
-    comment = forms.CharField(label="Комментарий")
+    comment = forms.CharField(label="Комментарий",
+    widget=forms.Textarea)
