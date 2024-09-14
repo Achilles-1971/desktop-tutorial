bb5e495d8755e59b32577648b7933e8dcdcd428a
Achilles, son of Peleus <bigboystar444@gmail.com>
2024-09-15 00:51:22 +0300

    Revert "Merge branch 'Учебная_Ветка' of https://github.com/Achilles-1971/desktop-tutorial into Учебная_Ветка"
    This reverts commit d3cb94278a595aef2aa56dede514ecc8ad062338, reversing
    changes made to 07aea77c5b4eedf30b248751b6bb20e2fa68e4c0.

--- web_1/hello/WEB1/views.py
+++ web_1/hello/WEB1/views.py
@@ -1,16 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render
 from django.template.response import TemplateResponse
+from django.template.response import TemplateResponse
 from django.http import *
-from WEB1.forms import UserForm
+from django.shortcuts import render
 def index(request):
-    userform = UserForm()
-    if request.method == "POST":
-        userform = UserForm(request.POST)
-        if userform.is_valid():
-            name = userform.cleaned_data["name"]
-            return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
-    return render(request, "WEB1/index.html", {"form": userform})
+    cat = []
+    return render(request, "WEB1/index.html", context={"cat": cat})
 def about(request):
     return HttpResponse("About")
 def contact(request):
