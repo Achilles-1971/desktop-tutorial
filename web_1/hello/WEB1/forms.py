ba414ba72e232032087c59a5dca1add7909e69cb
Achilles, son of Peleus <saikinairiofficial@gmail.com>
2024-09-14 18:14:25 +0300

    LZ9_18:14_7

--- web_1/hello/WEB1/forms.py
+++ web_1/hello/WEB1/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 class UserForm(forms.Form):
-    name = forms.CharField(label="Имя", help_text ="Введите ФИО")
-    age = forms.IntegerField(label="Возраст", help_text ="Введите возраст")
+    name = forms.CharField(label="Имя")
+    age = forms.IntegerField(min_value=1, max_value=120)
+    weight = forms.DecimalField(min_value=3, max_value=200, decimal_places=2)
+    email = forms.EmailField(label="Электронный адрес")
+    reklama = forms.BooleanField(label="Согласны получать рекламу")
