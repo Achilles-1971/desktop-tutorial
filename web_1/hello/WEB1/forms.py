23e68b3e8821f04a7e8106dba30447afb7933417
Achilles, son of Peleus <saikinairiofficial@gmail.com>
2024-09-14 18:03:32 +0300

    LZ9_18:03_1

--- web_1/hello/WEB1/forms.py
+++ web_1/hello/WEB1/forms.py
@@ -1,15 +1,5 @@
 from django import forms
 class UserForm(forms.Form):
-    file = forms.ImageField(label="Изображение")
-    date = forms.DateField(label="Введите дату")
-    time = forms.DateField(label="Введите время")
-    time_delta = forms.DurationField(label="Введите промежуток времени")
-    date_time = forms.SplitDateTimeField(label="Введите дату и время")
-    num = forms.IntegerField(label="Введите целое число")
-    num = forms.DecimalField(label="Введите десятичное число", decimal_places=2)
-    ling = forms.ChoiceField(label="Выберите язык", choices=((1, "Английский"),(2, "Немецкий"),
-    (3, "Французский")))
-    city = forms.TypedMultipleChoiceField(label="Выберите город",empty_value=None,choices=((1, "Москва"),
-    (2, "Воронеж"),(3, "Курск"),(4, "Томск")))
-    country = forms.MultipleChoiceField(label="Выберите страны",choices=((1, "Англия"),(2, "Германия"),
-    (3, "Испания"),(4, "Россия")))
+    name = forms.CharField(label="Имя")
+    age = forms.IntegerField(label="Возраст")
+    comment = forms.CharField(label="Комментарий")
